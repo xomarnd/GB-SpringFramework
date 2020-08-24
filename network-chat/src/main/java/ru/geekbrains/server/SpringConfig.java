4bbe60ab55c1552b15a6265301829785c80e0c00
smirnov <8c5LxlqoE>
2020-08-24 20:51:08 +0300

    lesson 1 @bean to properties

--- network-chat/src/main/java/ru/geekbrains/server/SpringConfig.java
+++ network-chat/src/main/java/ru/geekbrains/server/SpringConfig.java
@@ -1,7 +1,10 @@
 package ru.geekbrains.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import ru.geekbrains.server.auth.AuthService;
 import ru.geekbrains.server.auth.AuthServiceJdbcImpl;
@@ -11,7 +14,9 @@ import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Configuration
+@PropertySource("classpath:db.properties")
 public class SpringConfig {
+    Environment environment;
 
     @Bean
     public ChatServer chatServer() {
@@ -28,14 +33,26 @@ public class SpringConfig {
         return new UserRepository(dataSource);
     }
 
+    @Value("${jdbc.driver}")
+    private String jdbcDriver;
+
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${jdbc.username}")
+    private String username;
+
+    @Value("${jdbc.password}")
+    private String password;
+
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        ds.setUsername("kek");
-        ds.setPassword("keker");
-        ds.setUrl("jdbc:mysql://10.10.1.7:3306/kek?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
-        return ds;
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setUrl(jdbcUrl);
+        driverManagerDataSource.setUsername(username);
+        driverManagerDataSource.setPassword(password);
+        driverManagerDataSource.setDriverClassName(jdbcDriver);
+        return driverManagerDataSource;
     }
 
 }
